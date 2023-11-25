84809d63fa836c9f1dc946813e29b56335e582b6
Helena Kh <96687936+Elo2000@users.noreply.github.com>
2023-11-25 13:06:21 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+    path = require('path'),
+    routers = require('./routes/routes.js');
+
+
+const app = express();
+
+app.use('/list', express.static(path.join(__dirname, 'page/listDisplays.html')));
+app.use('/add_movie', express.static(path.join(__dirname, 'page/add_movie.html')));
+app.use('/add_actor', express.static(path.join(__dirname, 'page/add_actor.html')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', routers);
+
+const server = app.listen(3001, () => {
+    console.log('listening on port %s...', server.address().port);
+});
